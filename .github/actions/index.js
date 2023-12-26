fdd1d289d8a1ed312c649d4adc0835e3392d75b7
GabB40 <49393475+GabB40@users.noreply.github.com>
2023-12-26 14:07:23 +0100

    Create index.js

---
+++ .github/actions/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const { exec } = require('@actions/exec');
+
+async function run() {
+  try {
+    const toLog = core.getInput('tolog');
+    info(`*** [info] toLog: ${toLog}`);
+    core.debug(*** [debug] toLog: ${toLog}`)
+    
+    core.setOutput('my-output', 'Ploupi')
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
